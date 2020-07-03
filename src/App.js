cce8e76a62d06fdc17d96967d0166415ccf99a7d
varnits <varnit.sharan.phe16@itbhu.ac.in>
2020-07-03 21:54:57 +0530

    SPA-1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React , {Component}from 'react';
 import './App.css';
+import {BrowserRouter} from 'react-router-dom';
 import Main from './components/MainComponent.js';
 
 class  App extends Component{
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,17 @@
 import React , {Component}from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent.js';
 import {DISHES} from '../shared/dishes.js';
+import {COMMENTS} from '../shared/comments.js';
+import {LEADERS} from '../shared/leaders.js';
+import {PROMOTIONS} from '../shared/promotions.js';
+
+import Contact from './ContactComponent';
 import Footer from"./FooterComponent";
 
+import {Switch,Route, Redirect, BrowserRouter} from 'react-router-dom';
 import Dishdetail from './DishdetailComponent.js';
+
 import Header from"./HeaderComponent";;
 
 class  Main extends Component{
@@ -12,18 +20,20 @@ class  Main extends Component{
     this.state = {
     sk:"kahaan bhai",
     dishes:DISHES,
-    selectedDish:null  
+    comments: COMMENTS,
+    promotions: PROMOTIONS,
+    leaders: LEADERS,
     };
   }
-  onDishSelect(dishID){
-    this.setState({
-      selectedDish:dishID
-    });
-    console.log("se "+this.state.selectedDish);
-  // if(this.state.selectedDish!=null)
+  // onDishSelect(dishID){
+  //   this.setState({
+  //     selectedDish:dishID
+  //   });
+  //   console.log("se "+this.state.selectedDish);
+  // // if(this.state.selectedDish!=null)
  
-    //renderDish();
-  }
+  //   //renderDish();
+  // }
   renderDetails(){
     
     var dish =this.state.dishes.filter((dish)=>dish.id===this.state.selectedDish)[0];
@@ -38,16 +48,36 @@ class  Main extends Component{
         </div>
     );
   }
-  
+//   <Menu dishes={this.state.dishes}
+//   onClick={(dishID)=>this.onDishSelect(dishID)}
+// />
+// {this.renderDetails()} 
 render(){ 
+  const HomePage=()=>{
+    return (
+      <div>
+    <Home dish ={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo)=> promo.featured)[0]}
+          leader={this.state.leaders.filter((leader)=> leader.featured)[0]}
+    />
+      </div>
+    )
+   }
   return (
     <div >
+      <BrowserRouter>
       <Header/>
-      <Menu dishes={this.state.dishes}
-                onClick={(dishID)=>this.onDishSelect(dishID)}
-      />
-          {this.renderDetails()} 
+      
+        <Switch>
+          <Route path="/home" component={HomePage}/>
+          <Route exact path="/menu" component ={()=> <Menu dishes={this.state.dishes}/> }/>
+          <Route exact path='/contactus' component={Contact}  />
+
+          <Redirect to ="/home"/>
+        </Switch>
       <Footer/>
+
+      </BrowserRouter> 
       </div>
    
   );
